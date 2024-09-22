670c2f1955817b9fe579ef1943961877f99aecc3
markleduna321 <markharvey.leduna@gmail.com>
2024-09-22 08:50:51 +0800

    modal

--- resources/js/app/pages/admin/products/_sections/product-table-section.jsx
+++ resources/js/app/pages/admin/products/_sections/product-table-section.jsx
@@ -1,16 +1,19 @@
+import React, { useState } from 'react'
+import Modal from '@/app/components/modal'
+
+
 const people = [
-    { name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
-    { name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
-    { name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
-    { name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
-    { name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
-    { name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
-    // More people...
-  ]
-  
-  export default function ProductTableSection() {
-    return (
-      <div className="px-4 sm:px-6 lg:px-8">
+  { name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
+  // More people...
+]
+
+export default function ProductTableSection() {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => setModalOpen(true);
+  const closeModal = () => setModalOpen(false);
+  return (
+    <div className="px-4 sm:px-6 lg:px-8">
         <div className="sm:flex sm:items-center">
           <div className="sm:flex-auto">
             <h1 className="text-base font-semibold leading-6 text-gray-900">Products</h1>
@@ -23,10 +26,20 @@ const people = [
                 <button
                 type="button"
                 className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
+                onClick={openModal} >
                 Add product
                 </button>
-            
+
+                <Modal isOpen={isModalOpen} onClose={closeModal}>
+                  <h2 className="text-xl font-semibold mb-4">Modal Title</h2>
+                  <p>This is the content of the modal.</p>
+                  <button
+                    className="mt-4 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700"
+                    onClick={closeModal}
+                  >
+                    Close Modal
+                  </button>
+              </Modal>
           </div>
         </div>
         <div className="mt-8 flow-root">
@@ -74,5 +87,5 @@ const people = [
           </div>
         </div>
       </div>
-    )
-  }
+  )
+}
